291212693937778ed026df4cc0797bcb4a4db0d5
홍토끼 <54047794+revoirb612@users.noreply.github.com>
2020-12-22 16:58:48 +0900

    noun detector

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
+import pickle
 import pandas as pd
 import numpy as np
 from datetime import datetime
 
-df = pd.read_clipboard(sep=',')
-print(df)
+# 단어인가? 아닌가? 명사인가? 아닌가?
+
+thought = input('생각을 적어보시오: ')
+
+print('my answer :', thought)
+
+# 전처리
+thought = thought.strip('. ')
+words = thought.split()
+
+print('words : ', words)
+
+noun = []
+for x in words:
+    tmp = ''
+    while len(x):
+        print("'{}'은 명사입니까?".format(x))
+        answer = input('1/0 : ')
+        if answer == '1':
+            tmp = x
+            break
+        else:
+            x = x[:-1]
+    if len(tmp):
+        noun.append(tmp)
+
 
